53eaab1f6015b19c77480fe46005daee9828c275
Yashvanthk05 <yashvanthk05@gmail.com>
2024-09-11 20:40:19 +0530

    styling changed

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='p-6 pt-10 flex flex-row items-center justify-between min-w-full'>
         <ChevronLeft size={36}/>
-        <span className='font-bold text-3xl'>Leaderboard</span>
+        <span className='font-bold text-3xl text-black'>Leaderboard</span>
         <LayoutGrid size={36}/>
     </div>
   )
--- app/_components/Switch.jsx
+++ app/_components/Switch.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Switch = () => {
   return (
     <div className='bg-white rounded-md p-5 shadow-sm flex gap-12'>
-        <span className='text-xl item selected'>Organisation</span>
-        <span className='text-xl item'>Department</span>
+        <span className='text-xl item selected text-black'>Organisation</span>
+        <span className='text-xl item text-black'>Department</span>
     </div>
   )
 }
